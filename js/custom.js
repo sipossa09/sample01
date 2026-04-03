ae8aefa5ae802cef5da314aeeb555459bb21467c
sipossa09 <90168520+sipossa09@users.noreply.github.com>
2026-04-03 12:00:40 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,16 @@ $(function () {
     lucide.createIcons();
 
 
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.main_visual_slider', {
         loop: true,
+        autoplay: {
+            delay: 3000, // 3초
+            disableOnInteraction: false, // 클릭 후에도 계속 autoplay
+        },
+        effect: 'fade', // ⭐ 페이드 효과
+        fadeEffect: {
+            crossFade: true
+        },
     });
 
 
